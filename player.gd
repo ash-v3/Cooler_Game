ab9a093b46ac6a64ef0ec7b200ce98c03a90ccd6
Trevor_Vaughn <Trevor.N.Vaughn@gmail.com>
2021-07-13 10:07:12 -0700

    Player Rotation Init

--- player.gd
+++ player.gd
@@ -3,6 +3,10 @@ extends KinematicBody
 var velocity : Vector3 = Vector3.ZERO
 var acceleration : float = 1.5
 
+# The direction the player is looking
+# maybe: Vector2.RIGHT.rotate(angle)
+onready var look_vector = Vector2(global_transform.basis.x.x, global_transform.basis.z.z)
+
 func _ready():
 	pass
 
@@ -28,10 +32,37 @@ func _physics_process(delta):
 	# Rotate to face mouse
 	# Mouse position
 	var mouse_position = get_viewport().get_mouse_position()
-
-	# Project 3d position
-	var distance_from_camera = $Camera.translation.magnitude()
-	$Camera.project_position(mouse_position, distance_from_camera)
-
-	# Rotate the player to match the position
 	
+	# Extend a ray from the camera into infinity
+	var ray = $Camera.project_ray_normal(mouse_position)
+
+	# Player position
+	var player_y = global_transform.origin.y
+	var eyes_y = $Mesh/Nose.global_transform.origin.y
+	
+	# A plane with a normal vector that faces straight up
+	# Second argument is distance from origin that plane is
+	# Plane is ground plane raised to player's position
+	var plane = Plane(Vector3(0, 1, 0), eyes_y)
+	
+	# The point the ray and plane intersect
+	var intersection = plane.intersects_ray($Camera.global_transform.origin, ray)
+	
+	# Sometimes intersection is null, don't know why
+	if intersection:
+		# Get the normalized vector from the intersection point to the player
+		var mouse_vector = (intersection - global_transform.origin).normalized()
+		
+		# Convert vector into 2 dimensions for easier calculation
+		mouse_vector = Vector2(mouse_vector.x, mouse_vector.z)
+		
+		print(look_vector)
+
+		# The angle from the direction of the player is looking to the mouse
+		var angle = look_vector.angle_to(mouse_vector)
+		
+		# Rotate the mesh around the y axis to face the mouse
+		$Mesh.rotate(Vector3(0, 1, 0), -angle)
+		
+		# Save the new look vector
+		look_vector = mouse_vector
