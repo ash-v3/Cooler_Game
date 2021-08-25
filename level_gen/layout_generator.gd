e41a48f307b1c68a74dcff5bae1ed67b845680be
Trevor Vaughn <trevor.n.vaughn@gmail.com>
2021-08-24 22:01:53 -0700

    Path Generation

--- level_gen/layout_generator.gd
+++ level_gen/layout_generator.gd
@@ -10,17 +10,33 @@ var spread : int = 25 # spread
 
 var cull : float = 0.6 # chance of culling
 
+var path #AStar pathfinding object
+
 func _ready():
-	randomize()
 	make_rooms()
 
 func _process(delta):
 	update()
 
+func _draw():
+	draw_set_transform($Rooms.position, 0, Vector2(1, 1))
+
+	for room in $Rooms.get_children():
+		draw_rect(Rect2(room.position - room.size, room.size*2),
+					Color(0, 1, 0), false)
+	if path:
+		for p in path.get_points():
+			for c in path.get_point_connections(p):
+				var pp = path.get_point_position(p)
+				var cp = path.get_point_position(c)
+				draw_line(Vector2(pp.x, pp.y),
+							Vector2(cp.x, cp.y),
+							Color(1, 1, 0, 1), 15, true)
+
 func _input(event):
 	if event.is_action_pressed('ui_select'):
-		for n in $Rooms.get_children():
-			n.queue_free()
+		for room in $Rooms.get_children():
+			room.queue_free()
 		make_rooms()
 
 func make_rooms():
@@ -37,8 +53,22 @@ func make_rooms():
 	yield(get_tree().create_timer(1.5), 'timeout')
 	
 	# cull rooms
+	var room_positions = []
 	for room in $Rooms.get_children():
 		if randf() < cull:
 			room.queue_free()
-		#else:
-		#	room.mode = RigidBody2D.MODE_STATIC
+		else:
+			room.mode = RigidBody2D.MODE_STATIC
+			room_positions.append(Vector3(room.position.x, room.position.y, 0))
+		
+	yield(get_tree(), 'idle_frame')
+
+	# generate spanning tree (path)
+	path = TB.find_mst(room_positions)
+
+func get_rooms():
+	var rooms = []
+	for room in $Rooms.get_children():
+		rooms.append({"x": room.position.x, "y": room.position.y, "w": room.size.x, "h": room.size.y})
+	
+	return rooms
--- toolbox.gd
+++ toolbox.gd
@@ -28,3 +28,41 @@ func to_array(d):
 	for v in d:
 		a.append(v)
 	return a
+
+func find_mst(nodes):
+	# Prim's algorithm
+    # Given an array of positions (nodes), generates a minimum spanning tree
+    # Returns an AStar object
+
+    # Initialize the AStar and add the first point
+    var path = AStar.new()
+    path.add_point(path.get_available_point_id(), nodes.pop_front())
+
+    # Repeat until no more nodes remain
+    while nodes:
+        var min_dist = INF  # Minimum distance between any path node and any other node
+        var min_p = null  # Position of other node
+        var p = null  # Position on path closest to other node
+
+        # Loop through the points in the path
+        for p1 in path.get_points():
+            p1 = path.get_point_position(p1)
+
+			# Loop through the remaining nodes in the given array
+            for p2 in nodes:
+                # If the node is closer, make it the closest
+                if p1.distance_to(p2) < min_dist:
+                    min_dist = p1.distance_to(p2)
+                    min_p = p2
+                    p = p1
+
+		# Insert the resulting node into the path and add its connection
+        var n = path.get_available_point_id()
+        path.add_point(n, min_p)
+		# why get_closest_point()?
+        path.connect_points(path.get_closest_point(p), n)
+        
+		# Remove the node from the array so it isn't visited again
+        nodes.erase(min_p)
+
+    return path
